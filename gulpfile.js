a1892808f3edbb9db27e802b2122580ad6f131ab
Meghlaoui TouwfiQ <touwfiqdev@gmail.com>
2020-02-13 17:48:48 +0100

    install and setting up sass task autoprefixer cssnano gulp-sourcemaps gulp-postcss ,create watch sass task

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,44 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp = require("gulp"),
+    sass = require("gulp-sass"),
+    postcss = require("gulp-postcss"),
+    autoprefixer = require("autoprefixer"),
+    cssnano = require("cssnano"),
+    sourcemaps = require("gulp-sourcemaps");
+
+const sourcefile = './src/assets/scss/**/*.scss';
+const distfile = './dist/assets/css';
 
 
-gulp.task('sass', function () {
-    return gulp.src('./src/assets/scss/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./dist/assets/css'));
-});
+// gulp.task('sass', function () {
+//     return gulp.src('./src/assets/scss/**/*.scss')
+//         .pipe(sass().on('error', sass.logError))
+//         .pipe(autoprefixer())
+//         .pipe(sourcemaps.write('.'))
+//         .pipe(gulp.dest('./dist/assets/css'));
+// });
+
+function style() {
+    return (
+        gulp
+        .src(sourcefile)
+        // Initialize sourcemaps before compilation starts
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .on("error", sass.logError)
+        // Use postcss with autoprefixer and compress the compiled file using cssnano
+        .pipe(postcss([autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }), cssnano()]))
+        // Now add/write the sourcemaps
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(distfile))
+    );
+}
+exports.style = style;
+
+function watch() {
+    gulp.watch(sourcefile, style)
+}
+
+exports.watch = watch
